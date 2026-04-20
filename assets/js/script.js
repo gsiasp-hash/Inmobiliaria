336faeb59f1ceee8e02111b9a6288cef23af1358
Guillermo <g.siasp@gmail.com>
2026-04-19 20:11:58 -0400

    index

---
+++ assets/js/script.js
@@ -0,0 +1,180 @@
+// propiedades en alquiler
+const propiedades_alquiler = [
+  {
+    nombre: "Apartamento en el centro de la ciudad",
+    src: "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60",
+    descripcion: "Apartamento en el centro de la ciudad",
+    ubicacion: "123 Main Street, Downtown, CA 12345",
+    habitaciones: 2,
+    costo: 1500,
+    smoke: false,
+    pets: true,
+  },
+  {
+    nombre: "Apartamento luminoso con vista al mar",
+    src: "https://images.unsplash.com/photo-1669071192880-0a94316e6e09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    descripcion: "Apartamento luminoso con vista al mar",
+    ubicacion: "456 Ocean Avenue Seaside Town, CA 56789",
+    habitaciones: 3,
+    costo: 2500,
+    smoke: true,
+    pets: true,
+  },
+  {
+    nombre: "Apartamento en el centro de la ciudad",
+    src: "https://images.unsplash.com/photo-1567496898669-ee935f5f647a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvbmRvfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60",
+    descripcion: "Condominio moderno en zona residencial",
+    ubicacion: "123 Main Street, Downtown, CA 12345",
+    habitaciones: 2,
+    costo: 2200,
+    smoke: false,
+    pets: false,
+  },
+  {
+    nombre: "Apartamento en el centro de la ciudad",
+    src: "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60",
+    descripcion: "Apartamento en el centro de la ciudad",
+    ubicacion: "123 Main Street, Downtown, CA 12345",
+    habitaciones: 2,
+    costo: 1500,
+    smoke: false,
+    pets: true,
+  },
+  {
+    nombre: "Apartamento luminoso con vista al mar",
+    src: "https://images.unsplash.com/photo-1669071192880-0a94316e6e09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    descripcion: "Apartamento luminoso con vista al mar",
+    ubicacion: "456 Ocean Avenue Seaside Town, CA 56789",
+    habitaciones: 3,
+    costo: 2500,
+    smoke: true,
+    pets: true,
+  },
+  {
+    nombre: "Apartamento en el centro de la ciudad",
+    src: "https://images.unsplash.com/photo-1567496898669-ee935f5f647a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvbmRvfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60",
+    descripcion: "Condominio moderno en zona residencial",
+    ubicacion: "123 Main Street, Downtown, CA 12345",
+    habitaciones: 2,
+    costo: 2200,
+    smoke: false,
+    pets: false,
+  },
+];
+// propiedades en venta
+const propiedades_venta = [
+  {
+    nombre: "Apartamento de lujo en zona exclusiva",
+    src: "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
+    descripcion:
+      "Este apartamento de lujo está ubicado en una exclusiva zona residencial",
+    ubicacion: "123 Luxury Lane, Prestige Suburb, CA 45678",
+    habitaciones: 4,
+    costo: 5000,
+    smoke: false,
+    pets: false,
+  },
+  {
+    nombre: "Apartamento acogedor en la montaña",
+    src: "https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg",
+    descripcion:
+      "Este apartamento acogedor está situado en lo alto de una montaña con impresionantes vistas",
+    ubicacion: "789 Mountain Road, Summit Peaks, CA 23456",
+    habitaciones: 2,
+    costo: 1200,
+    smoke: true,
+    pets: true,
+  },
+  {
+    nombre: "Penthouse de lujo con terraza panorámica",
+    src: "https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg",
+    descripcion:
+      "Este penthouse de lujo ofrece una terraza con vistas panorámicas",
+    ubicacion: "456 Panorama Drive, City Center, CA 12345",
+    habitaciones: 3,
+    costo: 3500,
+    smoke: true,
+    pets: true,
+  },
+  {
+    nombre: "Apartamento de lujo en zona exclusiva",
+    src: "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
+    descripcion:
+      "Este apartamento de lujo está ubicado en una exclusiva zona residencial",
+    ubicacion: "123 Luxury Lane, Prestige Suburb, CA 45678",
+    habitaciones: 4,
+    costo: 5000,
+    smoke: false,
+    pets: false,
+  },
+  {
+    nombre: "Apartamento acogedor en la montaña",
+    src: "https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg",
+    descripcion:
+      "Este apartamento acogedor está situado en lo alto de una montaña con impresionantes vistas",
+    ubicacion: "789 Mountain Road, Summit Peaks, CA 23456",
+    habitaciones: 2,
+    costo: 1500,
+    smoke: true,
+    pets: true,
+  },
+  {
+    nombre: "Penthouse de lujo con terraza panorámica",
+    src: "https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg",
+    descripcion:
+      "Este penthouse de lujo ofrece una terraza con vistas panorámicas",
+    ubicacion: "456 Panorama Drive, City Center, CA 12345",
+    habitaciones: 3,
+    costo: 3500,
+    smoke: true,
+    pets: true,
+  },
+];
+
+// funcion para mostrar propiedades en la página
+function mostrarPropiedades(propiedades, contenedorId) {
+  const contenedor = document.getElementById(contenedorId);
+  contenedor.innerHTML = "";
+
+  propiedades.forEach((propiedad) => {
+    const propiedadHTML = `
+<div class="col-md-4 mb-4">
+            <div class="card">
+              <img
+                src="${propiedad.src}"
+                class="card-img-top"
+                alt="${propiedad.nombre}"
+              />
+              <div class="card-body">
+                <h5 class="card-title">
+                  ${propiedad.nombre}
+                </h5>
+                <p class="card-text">
+                  ${propiedad.descripcion}
+                </p>
+                <p>
+                  <i class="fas fa-map-marker-alt"></i> ${propiedad.ubicacion}
+                </p>
+                <p>
+                  <i class="fas fa-bed"></i> ${propiedad.habitaciones} Habitaciones
+                </p>
+                <p><i class="fas fa-dollar-sign"></i> ${propiedad.costo}</p>
+                <p class="${propiedad.smoke ? "text-success" : "text-danger"}">
+                  <i class="fas fa-${propiedad.smoke ? "smoking" : "smoking-ban"}"></i> ${propiedad.smoke ? "Permitido fumar" : "No se permite fumar"}
+                </p>
+                <p class="${propiedad.pets ? "text-success" : "text-danger"}">
+                  <i class="fa-solid fa-${propiedad.pets ? "paw" : "ban"}"></i> ${propiedad.pets ? "Mascotas permitidas" : "No se permiten mascotas"}
+                </p>
+              </div>
+            </div>
+          </div>`;
+    contenedor.innerHTML += propiedadHTML;
+  });
+}
+
+// Mostrar propiedades de venta y alquiler al cargar la página
+mostrarPropiedades(propiedades_venta.slice(0, 3), "propiedades-contenedor");
+mostrarPropiedades(
+  propiedades_alquiler.slice(0, 3),
+  "propiedades-contenedor-alquiler",
+);
